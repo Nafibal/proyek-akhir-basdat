8c8606c034105989e178eacd1f74e2662c59d8f1
mafibal <baliya363@gmail.com>
2021-11-21 17:54:26 +0700

    browse page done

--- pages/navigation.php
+++ pages/navigation.php
@@ -1,18 +1,23 @@
 <nav class="alternate">
-    <div class="container">
+      <div class="container">
         <img src="../assets/img/logo-alt.png" alt="log0" />
         <div class="btn-container">
-            <button class="btn btn-alt-2">
-                <a href="../logout.php">logout</a>
-            </button>
+          <button class="btn btn-cart">
+            <a href=""><i class="fas fa-shopping-cart"></i>
+                <?php
+                    if (isset($_SESSION['cart'])) {
+                        $count = count($_SESSION["cart"]);
+                        echo "<span id=\"cart_count\">$count</span>";
+                    } else {
+                        echo "<span id=\"cart_count\">0</span>";
+                    }
+                ?>
+            </a>
+          </button>
+          <button class="btn btn-alt-2">
+            <a href="../logout.php">logout</a>
+          </button>
         </div>
-        <?php
-            if (isset($_SESSION['cart'])) {
-                $count = count($_SESSION["cart"]);
-                echo "<span id=\"cart_count\">$count</span>";
-            } else {
-                echo "<span id=\"cart_count\">0</span>";
-            }
-        ?>
-    </div>
-</nav>
+      </div>
+    </nav>
+
